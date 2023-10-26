29501a4d5bdd06472c3b722e06b0a7082a8cb8b3
Justin Huynh <justinhuynh639@gmail.com>
2023-10-25 23:58:55 -0700

    timer down

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ def alarm(seconds):
     time_elapsed = 0
 
     while time_elapsed < seconds:
+        time.sleep(1)
+        time_elapsed += 1
+
+        time_left = seconds = time_elapsed
+        minutes_left = time_left // 60
+        seconds_left = time_left % 60
+
+        print(f"{minutes_left}:{seconds_left}")
+
+alarm(10)
